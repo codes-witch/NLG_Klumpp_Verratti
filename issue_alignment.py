8e068213734f88c1377c179cd734774365a1ee9e
janfelixk <56627638+janfelixk@users.noreply.github.com>
2023-03-27 15:22:57 +0200

    Create issue_alignment.py

--- issue_alignment.py
+++ issue_alignment.py
@@ -10,7 +10,10 @@ import json
 from evaluation import BirdDistractorDataset
 from rsa_eval import KeywordClassifier
 
+
 # evaluate issue alignment for all captions in a file
+# this weights all image-issue pairs equally (in contrast to evaluate_captions_by_image(), which calculates the 
+# average precision and recall for each image individually)
 def evaluate_captions(path):
 
     # instantiate the keyword classifier
@@ -61,9 +64,78 @@ def evaluate_captions(path):
     # return a tuple (precision, recall)
     return precision, recall
 
+
+# alternative evaluation method
+# here, image-issue pairs are not weighted equally, but instead, precision and recall are calculated for each image 
+# and then averaged
+def evaluate_captions_by_image(path):
+
+    # instantiate the keyword classifier
+    rsa_dataset = BirdDistractorDataset(randomized=True)
+    kc = KeywordClassifier(rsa_dataset)
+
+    # all issues to be looked at
+    issues = [('wing', 'pattern'), ('belly', 'pattern'), ('breast', 'pattern'), ('nape', 'color'),
+                ('upper_tail', 'color'), ('under_tail', 'color'), ('back', 'color'), ('leg', 'color'),
+                ('throat', 'color'), ('crown', 'color'), ('bill', 'shape'), ('eye', 'color'), ('wing', 'color'),
+                ('bill', 'color'), ('breast', 'color'), ('belly', 'color'), ('bill', 'length')]
+
+    sum_true_pos = 0  # target issue is addressed
+    sum_false_neg = 0  # target issue is not addressed
+    sum_false_pos = 0  # number of other issues that are addressed
+    precisions = [] # list of precisions for each image
+    recalls = [] # list of recalls for each image
+    
+    # load captions
+    with open(path) as input_file:
+        captions = json.loads(input_file.read())
+
+    # go through all images
+    for img in captions:
+        # go through all captions for that image and issue
+        for issue in captions[img]:
+            cap = captions[img][issue]
+
+            # parts and aspects can be extracted from the list of issues
+            part = issues[int(issue)][0]
+            aspect = issues[int(issue)][1]
+
+            # check whether the issue is resolved
+            if kc.classify_parts_aspect(part, aspect, cap, tokenize=True):
+                sum_true_pos+=1
+            else:
+                sum_false_neg+=1
+
+            # go through all other issues in the list that should not be resolved by the caption
+            for other_i in range(17):
+                if other_i != int(issue):
+                    other_part = issues[other_i][0]
+                    other_aspect = issues[other_i][1]
+                    if kc.classify_parts_aspect(other_part, other_aspect, cap, tokenize=True):
+                        sum_false_pos += 1
+
+        # if there is at least one resolvable issue for the image, calculate precision and recall
+        if len(captions[img])>0:
+            precisions.append(sum_true_pos/(sum_true_pos+sum_false_pos))
+            recalls.append(sum_true_pos/(sum_true_pos+sum_false_neg))
+        # set all sums back to 0
+        sum_true_pos=0
+        sum_false_neg=0
+        sum_false_pos=0
+
+    # overall precision and recall are the average of the scores values for individual images
+    precision = sum(precisions)/len(precisions)
+    recall = sum(recalls)/len(recalls)
+
+    # return a tuple (precision, recall)
+    return precision, recall
+
+
 if __name__ == '__main__':
 
     # get scores for all conditions
+    # the default condition is equal weighting of all image-issue pairs
+    # for equal weighting of all images, change to evaluate_captions_by_image()
     s0 = evaluate_captions("./results/random_run_0/S0_gen_captions.json")
     s1 = evaluate_captions("./results/random_run_1/S1_gen_captions.json")
     s1c = evaluate_captions("./results/random_run_2/S1_C_gen_captions.json")
