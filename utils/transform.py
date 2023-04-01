25147dae80462fb1baa5dea3e48c89671860d44f
codes-witch <daniverratti@gmail.com>
2023-04-01 23:18:06 +0200

    minor in-line comments

--- utils/transform.py
+++ utils/transform.py
@@ -7,13 +7,15 @@ def get_transform(net, train=True):
     """
     if net is None:
         transform = None
-    elif net.startswith('vgg'):
+
+    elif net.startswith('vgg'): # the only possible model we can use is vgg
         if train:
             transform = transforms.Compose([transforms.Resize(256),
                                             transforms.RandomCrop(224),
                                             transforms.RandomHorizontalFlip(),
                                             transforms.ToTensor(),
                                             transforms.Normalize(mean=(0.485, 0.456, 0.406),
+
                                                                  std=(0.229, 0.224, 0.225))])
         else:
             transform = transforms.Compose([transforms.Resize(224),
