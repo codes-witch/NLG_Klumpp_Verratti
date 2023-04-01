7b28f400d89311d211c8fb4a7f8a8f156dbcc965
codes-witch <daniverratti@gmail.com>
2023-04-01 23:02:30 +0200

    added minor comments

--- utils/logger.py
+++ utils/logger.py
@@ -2,6 +2,9 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 class Logger(object):
+    """
+    This code has been left untouched.
+    """
 
     def __init__(self, log_dir):
         """Create a summary writer logging to log_dir."""
@@ -9,4 +12,6 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
+        # OWN COMMENT: This is used for logging epoch and batch loss in main_notes.py as well as in the LCRN and
+        # SentenceClassifier trainers
         self.writer.add_scalar(tag, value, step)
