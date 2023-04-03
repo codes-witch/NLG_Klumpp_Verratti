48415e1481a269810819fd5799899d587e8e2824
codes-witch <daniverratti@gmail.com>
2023-04-03 21:20:14 +0200

    final

--- models/pretrained_models.py
+++ models/pretrained_models.py
@@ -3,10 +3,11 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torchvision.models
 
-""" This class is used to load pretrained models that are very popular for image recognition (resnet and vgg). """
-
 
 class PretrainedModel(nn.Module):
+    """
+    This class is used to load pretrained models that are very popular for image recognition (resnet and vgg).
+    """
     # define error messages
     ERR_TRUNC_MSG = ("{} currently only supports to be truncated "
                      "by its last {} FC layer(s). Please choose a value "
@@ -22,7 +23,9 @@ class PretrainedModel(nn.Module):
 
 
     def __init__(self, model_name, layers_to_truncate=1):
-        """Original Comment: Load the pretrained ResNet-152 and replace top fc layer."""
+        """
+        Original Comment: Load the pretrained ResNet-152 and replace top fc layer.
+        """
         super(PretrainedModel, self).__init__()
 
         # if the model is not one of the expected ones, raise error
@@ -54,7 +57,6 @@ class PretrainedModel(nn.Module):
             # use model without truncation
             self.pretrained_model = model
         else:
-            # TODO (Original) Truncate last FC layer(s)
             # get the layers for each model
             if model_name.startswith('vgg'):
                 layers = list(model.classifier.children())
@@ -62,37 +64,41 @@ class PretrainedModel(nn.Module):
                 layers = list(model.children())
 
             trunc = self._get_num_truncated_layers(layers_to_truncate, layer_size)
-            last_layer = layers[trunc]
 
-            # Original Comment: Delete the last layer(s).
+            # truncation of unwanted layers
             modules = layers[:trunc]
+
+            # define sequence for modules to be applied
             if model_name.startswith('vgg'):
                 self.pretrained_model = model
                 self.pretrained_model.classifier = nn.Sequential(*modules)
             else:
                 self.pretrained_model = nn.Sequential(*modules)
 
-        # Original Comment: Freeze all parameters of pretrained model
+        # we do not train the CNN model - we do not require gradients
         for param in self.pretrained_model.parameters():
             param.requires_grad = False
 
         # Original Comment: Switch model to eval mode (affects Dropout & BatchNorm)
         self.pretrained_model.eval()
 
+        # set output size
         self.output_size = self._get_output_size()
 
     def _get_output_size(self):
-        # get the output size by passing a dummy input through the model and inspecting the output
+        """
+        Get the output size by passing a dummy input through the model and inspecting the output.
+
+        """
         dummy_input = Variable(torch.rand(1, *self.input_size))
         output = self(dummy_input)
         output_size = output.data.view(-1).size(0)
         return output_size
 
     def _get_num_truncated_layers(self, num_to_trunc, layer_size, initial_layer_size=1):
-
-        # get the number of truncated numbers by adding layer_size until num_to_trunc is 0
-        # TODO: I do not completely understand what layer_size is, do you have an idea?
-        # TODO: It is the number of fully-connected layers at the end of the VGG and ResNet architectures, I think.
+        """
+        get the number of truncated numbers by adding layer_size until num_to_trunc is 0
+        """
         num = 0
         if num_to_trunc > 0:
             num += initial_layer_size
@@ -100,13 +106,15 @@ class PretrainedModel(nn.Module):
         while num_to_trunc > 0:
             num += layer_size
             num_to_trunc -= 1
-        # return a negative number because this is used as an index until which the layers are preserved
+        # return a negative number because this is used as the index until which the layers are preserved
         return -num
 
     def forward(self, images):
-        """Original Comment: Extract the image feature vectors."""
-        # TODO can it be deleted?
+        """
+        Original Comment: Extract the image feature vectors.
+        """
         features = self.pretrained_model(images)
+        # to facilitate backpropagation
         features = Variable(features.data)
         features = features.view(features.size(0), -1)
         return features
