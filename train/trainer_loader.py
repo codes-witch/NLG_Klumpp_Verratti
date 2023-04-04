06fbf337eabbc377bc7c34d7da286303e7f17e4a
codes-witch <daniverratti@gmail.com>
2023-04-04 17:54:47 +0200

    final

--- train/trainer_loader.py
+++ train/trainer_loader.py
@@ -3,6 +3,9 @@ from .gve_trainer import GVETrainer
 from .sentence_classifier_trainer import SCTrainer
 
 class TrainerLoader:
+    """
+    Loading the trainer classes for LRCN, SentenceClassifier and GVE
+    """
     lrcn = LRCNTrainer
     gve = GVETrainer
     sc = SCTrainer
